d8b4bf4c0219a293d1d6faa74060da6027f8b4bf
root <a08cbc90@fpso.dip.jp>
2021-04-02 09:13:38 +0000

    add print_monthly_summary

--- ieyasu.py
+++ ieyasu.py
@@ -50,25 +50,83 @@ class Yeyasu():
         self.request = self.session.get(url=self.login_url)
         self.bs = bs4.BeautifulSoup(self.request.text, 'html5lib')
         self.form = self.bs.select_one('form#new_user')
-        self.post = {}
+        self.login_form = {}
         for tag in self.form.select('[name]'):
             if tag.has_attr('value'):
-                self.post[tag['name']] = tag['value']
+                self.login_form[tag['name']] = tag['value']
             else:
-                self.post[tag['name']] = ''
-        self.post['user[login_id]'] = self.user
-        self.post['user[password]'] = self.password
-        print(self.form['action'])
+                self.login_form[tag['name']] = ''
+        self.login_form['user[login_id]'] = self.user
+        self.login_form['user[password]'] = self.password
+        #print(self.form['action'], self.login_form)
 
         self.login_url = self.relative_path_to_url(self.form['action'])
-        return self.post
+        return self.login_form
 
     def login(self):
-        self.request = self.session.post(url=self.login_url, data=self.post)
-        self.bs = bs4.BeautifulSoup(self.request.text, 'html5lib')
-        print(self.bs.prettify())
-        
-    def dev(self):
+        self.create_post_from_toppage()
+        return bs4.BeautifulSoup(
+                self.session.post(url=self.login_url, data=self.login_form).text,
+                'html5lib')
+
+    def print_monthly_summary(self, date="2021/04"):
+        """
+        date format:
+            yyyy.mm[.dd]
+        """
+        _r = re.search('^\s*([0-9]{4})[^0-9]([0-9]{2})', date)
+        if not _r:
+            return {}
+        yyyy = _r.group(1)
+        mm =_r.group(2)
+        _l = {}
+        url = self.site_root + "/works/%s-%s" % (yyyy, mm)
+        #print(url, self.site_root + "/works/%s-%s" % (yyyy, mm))
+
+        self.bs = bs4.BeautifulSoup(self.session.get(url=url).text, 'html5lib')
+        #print(self.bs.prettify())
+        summary = {}
+        for _i in self.bs.form.select('table tr'):
+            # date
+            _d = _i.select_one('td.cellDate span.date')
+            # href
+            _h = _i.select_one('td.cellDate div.view_work a[href]')
+            if not _h:
+                _h = "UnEditable or Accepted"
+            else:
+                _h = self.site_root + "/" + _h['href'].strip()
+            # start
+            _s = _i.select_one('td.cellTime.cellTime01.cellBreak.view_work div.item01 span')
+            # end
+            _e = _i.select_one('td.cellTime.cellTime02.view_work div.item01')
+            # break time
+            _b = _i.select_one('td.cellTime.cellTime07.cellBtime.view_work')
+            # total time
+            _t = _i.select_one('td.cellTime.cellTime08.view_work')
+            #print(_s, _e, _b, _t)
+            if not (_d and _s and _e and _b and _t):
+                # lack date or href or ....
+                continue
+            _d = _d.text.strip()
+            _s = _s.text.strip()
+            _e = _e.text.strip()
+            _b = _b.text.strip()
+            _t = _t.text.strip()
+            summary[_d] = {'link': _h, 'start': _s, 'end': _e, 'break': _b, 'total': _t,}
+        #print(summary)
+        print('Year,%s,Month,%s,'  % (yyyy, mm))
+        print('Date,Start,End,Breaks,Total,Link')
+        for _i in sorted(summary.keys()):
+            print('%s,%s,%s,%s,%s,%s' % (
+                _i,
+                summary[_i]['start'],
+                summary[_i]['end'],
+                summary[_i]['break'],
+                summary[_i]['total'],
+                summary[_i]['link']))
+        return summary
+
+    def dev(self, command=''):
         '''
         request format:
             [YYYY/MM/][D]D,[k|[int],[int][,int]][,][# comments]
@@ -78,20 +136,78 @@ class Yeyasu():
             2021/04/03,,            # do nothing
             4,                      # do nothing
             2021/04/05,k            # set rest day
+
+
+        _u = "https://"
+        _b = bs4.BeautifulSoup(self.session.get(url=_u).text, 'html5lib')
+        print(_b.prettify())
+        exit()
         '''
 
-        with open('sample') as fh:
+
+        with open('../sample') as fh:
             self.bs = bs4.BeautifulSoup(fh.read(), 'html5lib')
-        print(self.bs.form)
+        _f = self.bs.select_one('form')
+        print(_f.prettify())
+        # action
+        action = _f["action"]
+        # inputs
+        form = {}
+        for tag in _f.select('[name]'):
+            print(tag.prettify())
+            # if statements didnt work....
+            if tag.select_one('input[type=hidden][name][value]'):
+                # input[hidden]
+                print("added", tag['name'], tag.prettify())
+                form[tag['name']] = tag.select('input[type=hidden][name][value]')['value']
+            elif tag.select_one('select option[selected=selected][value]'):
+                # select option[selected]
+                print("added", tag['name'], tag.prettify())
+                form[tag['name']] = tag.select_one('select option[selected=selected][value]')['value']
+            elif tag.select_one('input[type=text][value]'):
+                # input[text]
+                print("added", tag['name'], tag.prettify())
+                form[tag['name']] = tag.select_one('input[type=text][value]')['value']
+            else:
+                print("ERR", tag.prettify())
+                form[tag['name']] = ''
+        #form[''] = 
+        print(form)
+
+        exit()
         action_url = self.relative_path_to_url(self.bs.form['action'])
         # current YYYY
-        yyyy = re.search('=([0-9]{4})-', action_url).group(1)
+        yyyy = re.search('/([0-9]{4})-', action_url).group(1)
         # current MM
         mm = re.search('-([0-9]{2})$', action_url).group(1)
-
-        print(action_url)
+        print(self.bs.select_one('form').prettify())
         #print(self.bs.form.select('table tr'))
         hoplist = {}
+        for i in self.bs.form.select('table tr'):
+            # date
+            _d = i.select_one('td.cellDate span.date')
+            # href
+            _h = i.select_one('td.cellDate div.view_work a[href]')
+            # start
+            _s = i.select_one('td.cellTime.cellTime01.cellBreak.view_work div.item01 span')
+            # end
+            _e = i.select_one('td.cellTime.cellTime02.view_work div.item01')
+            # break time
+            _b = i.select_one('td.cellTime.cellTime07.cellBtime.view_work')
+            # total time
+            _t = i.select_one('td.cellTime.cellTime08.view_work')
+            #print(_s, _e, _b, _t)
+            if not (_d and _h and _s and _e and _b and _t):
+                # lack date or href or ....
+                continue
+            _d = _d.text.strip()
+            _h = _h['href'].strip()
+            _s = _s.text.strip()
+            _e = _e.text.strip()
+            _b = _b.text.strip()
+            _t = _t.text.strip()
+            print(_d, _s, _e, _b, _t, _h)
+        """
         for i in self.bs.form.select('table tr'):
             # date
             _d = i.select_one('td.cellDate span.date')
@@ -102,6 +218,7 @@ class Yeyasu():
             if not _h:
                 continue
             print(_d.text.strip(), _h['href'].strip())
+        """
 
 
 
@@ -110,9 +227,11 @@ y = Yeyasu(
         url=c['url'],
         user=c['usr'],
         password=c['pas'])
-'''
-y.create_post_from_toppage()
-print(y.site_root,y.post)
-y.login()
-'''
-y.dev()
+"""
+print(y.login())
+y.print_monthly_summary("2021-04")
+"""
+y.dev("2021-04")
+exit()
+
+#y.dev()
